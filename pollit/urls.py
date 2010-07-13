c5349a4100fec6bb8cbd1b2ea70c365ab18d68a0
Justin Quick <justquick@gmail.com>
2010-07-13 11:47:59 -0400

    added generic view for poll comments

--- pollit/urls.py
+++ pollit/urls.py
@@ -1,13 +1,25 @@
 from django.conf.urls.defaults import *
+from models import Poll
 
-urlpatterns = patterns('pollit.views',
+comment_info_dict = {
+    'queryset': Poll.objects.filter(status__lt=3, comment_status__gt=1),
+    'template_object_name': 'poll',
+    'date_field': 'pub_date',
+    'template_name': 'pollit/comments.html'
+}
+
+urlpatterns = patterns('',
     url(r'^$',
-        'index',
+        'pollit.views.index',
         name='pollit_index'),
     url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<slug>[\w-]+)/$',
-        'detail',
+        'pollit.views.detail',
         name='pollit_detail'),
     url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<slug>[\w-]+)/results/$',
-        'results',
-        name='pollit_results')
+        'pollit.views.results',
+        name='pollit_results'),
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\d{1,2})/(?P<slug>[\w-]+)/comments/$',
+        'django.views.generic.date_based.object_detail',
+        kwargs=comment_info_dict,
+        name='pollit_comments')
 )
