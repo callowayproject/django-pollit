0bf7b2f53c43a8b423768cd39bac0204ed35503b
Corey Oordt <coordt@washingtontimes.com>
2010-09-01 06:49:56 -0400

    Added some tests

---
+++ pollit/tests.py
@@ -0,0 +1,75 @@
+from pollit.models import Poll, PollChoice, PollChoiceData, AlreadyVoted, PollExpired
+import datetime
+from django.contrib.auth.models import User
+from django.http import Http404
+
+from django.test import TestCase
+
+class TestPollit(TestCase):
+    """Tests for Pollit"""
+    def setUp(self):
+
+        self.poll = Poll.objects.create(
+            question="How are you feeling?",
+            pub_date=datetime.datetime.now(),
+        )
+        self.poll.save()
+
+        self.poll.pollchoice_set.create(choice="Yes")
+        self.poll.pollchoice_set.create(choice="No")
+        self.poll.pollchoice_set.create(choice="What")
+
+        self.user = User.objects.create_user('demouser','demo@example.com')
+
+    def testVoting(self):
+        """
+        Test the basic Poll.vote method
+        """
+        selected_choice_id = self.poll.pollchoice_set.all()[1].id
+        self.poll.vote(selected_choice_id, self.user)
+        self.assertEquals(self.poll.total_votes, 1)
+        
+        for choice in self.poll.pollchoice_set.all():
+            if choice.id == selected_choice_id:
+                self.assertEquals(choice.votes, 1)
+            else:
+                self.assertEquals(choice.votes, 0)
+    
+    def testVoteOnlyOnce(self):
+        """
+        Test that a user can only vote once
+        """
+        selected_choice_id = self.poll.pollchoice_set.all()[1].id
+        self.poll.vote(selected_choice_id, self.user)
+        selected_choice_id = self.poll.pollchoice_set.all()[0].id
+        self.assertRaises(AlreadyVoted, self.poll.vote, selected_choice_id, self.user)
+    
+    
+    def testPollExpired(self):
+        """
+        The poll is expired if the status is 3 or
+        if the status is 2 and IF there is an expire_date, it has not occurred.
+        """
+        # default is 2, which is always valid
+        selected_choice_id = self.poll.pollchoice_set.all()[0].id
+        self.poll.vote(selected_choice_id, self.user)
+        
+        #status 3 is invald (closed)
+        self.poll.status = 3
+        self.poll.save()
+        user = User.objects.create_user('demouser4','demo4@example.com')
+        self.assertRaises(PollExpired, self.poll.vote, selected_choice_id, user)
+        
+        # status 1, but no expire date is valid
+        self.poll.status = 1
+        self.poll.save()
+        user2 = User.objects.create_user('demouser2','demo2@example.com')
+        self.poll.vote(selected_choice_id, user2)
+        
+        # status 1, and now has an expire_date
+        self.poll.expire_date = datetime.datetime.now()
+        self.poll.save()
+        user3 = User.objects.create_user('demouser3','demo3@example.com')
+        selected_choice_id = self.poll.pollchoice_set.all()[0].id
+        self.assertRaises(PollExpired, self.poll.vote, selected_choice_id, user3)
+        
