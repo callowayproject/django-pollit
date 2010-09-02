8be78e9fed8710227443b8a673f56842bff3ebce
Corey Oordt <coordt@washingtontimes.com>
2010-09-02 06:08:32 -0400

    Updated the Poll model and added the vote method

--- pollit/models.py
+++ pollit/models.py
@@ -67,11 +67,14 @@ class PollManager(models.Manager):
 class Poll(models.Model):
     question = models.CharField(max_length=255)
     slug = models.SlugField()
-    pub_date = models.DateTimeField(blank=True, null=True)
-    types = models.ManyToManyField(PollType, null=True, blank=True, related_name="polls")
-    sites = models.ForeignKey(Site, related_name="polls")
+    pub_date = models.DateTimeField(auto_now_add=True)
+    sites = models.ManyToManyField(Site, 
+        null=True, 
+        blank=True, 
+        related_name="polls")
     status = models.PositiveIntegerField(default=2, choices=POLL_STATUS)
     expire_date = models.DateTimeField(blank=True, null=True)
+    total_votes = models.IntegerField(editable=False, default=0)
     comment_status = models.IntegerField('Comments', 
         default=3, choices=POLL_COMMENT_STATUS)
     
@@ -82,51 +85,110 @@ class Poll(models.Model):
     
     def __unicode__(self):
         return self.question
-        
+    
+    @permalink
     def get_absolute_url(self):
+        print self.pub_date.year, self.pub_date.month, self.pub_date.day
         return ('pollit_detail', None, {
-                'year': self.pub_date.strftime("%Y"),
-                'month': self.pub_date.strftime("%b").lower(),
+                'year': self.pub_date.year,
+                'month': self.pub_date.strftime('%b').lower(),
+                'day': self.pub_date.day,
                 'slug': self.slug })
-    get_absolute_url = permalink(get_absolute_url)  
+    
+    @permalink
+    def get_absolute_results_url(self):
+        return ('pollit_results', None, {
+                'year': self.pub_date.year,
+                'month': self.pub_date.month,
+                'day': self.pub_date.day,
+                'slug': self.slug })
+    
+    @permalink
+    def get_absolute_comments_url(self):
+        return ('pollit_results', None, {
+                'year': self.pub_date.year,
+                'month': self.pub_date.month,
+                'day': self.pub_date.day,
+                'slug': self.slug })
     
     def user_can_vote(self, user):
+        if not user.is_authenticated():
+            return False
         try:
             PollChoiceData.objects.get(poll__pk=self.pk, user__pk=user.pk)
             return False
-        except:
+        except PollChoiceData.DoesNotExist:
             return True
-            
+    
     def is_expired(self):
-        if not self.pub_date:
-            return True
-        if not expire_date and status [1, 2]:
+        """
+        Check if the poll has expired. This is True if the status is 3
+        or if the status is 1 and there is no expire date, or the expire
+        date has not occured
+        """
+        if self.status == 2:
             return False
-        if status == 3:
+        elif self.status == 3:
             return True
-            
-        if self.expire_date and self.expire_date <= datetime.datetime.now():
-            return True
-            
-    def publish(self):
-        self.pub_date = datetime.datetime.now()
+        else:
+            if self.expire_date is None:
+                return False
+            elif self.expire_date <= datetime.datetime.now():
+                return True
+    
+    def vote(self, choice, user):
+        """
+        Vote on a poll.
+        
+        Does all the checks for duplication
+        
+        :param choice: The id, or :class:`PollChoice` voted
+        :type choice:  int or :class:`PollChoice`
+        :param user:   The user who voted.
+        :type user:    A Django ``User`` instance
+        """
+        if not self.user_can_vote(user):
+            raise AlreadyVoted()
+        
+        if self.is_expired():
+            raise PollExpired()
+        
+        try:
+            if isinstance(choice, PollChoice):
+                selected_choice = choice
+            else:
+                selected_choice = PollChoice.objects.get(pk=choice)
+        except PollChoice.DoesNotExist:
+            raise Http404("Selected choice does not exist")
+        
+        PollChoiceData.objects.create(
+            poll=self, 
+            choice=selected_choice, 
+            user=user)
+        
+        selected_choice.votes += 1
+        selected_choice.save()
+        self.total_votes += 1
         self.save()
-        
-        
+
+
 class PollChoice(models.Model):
+    """
+    Choices for polls. Choices are referenced by their own unique id for voting.
+    """
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=255)
     votes = models.IntegerField(editable=False, default=0)
     order = models.IntegerField(default=1)
     
     def percentage(self):
-        if not self.votes:
+        """
+        The percentage of the total votes in the poll that choce this option
+        """
+        total = self.poll.total_votes
+        if total == 0 or self.votes == 0:
             return 0
-            
-        total = PollChoice.objects.filter(poll=self.poll).aggregate(Sum('votes'))
-        if total['votes__sum'] == 0:
-            return 0
-        return int((float(self.votes) / float(total['votes__sum'])) * 100)
+        return int((float(self.votes) / float(total)) * 100)
     
     def __unicode__(self):
         return self.choice
@@ -136,6 +198,13 @@ class PollChoice(models.Model):
         
         
 class PollChoiceData(models.Model):
+    """
+    A User's vote on a poll
+    """
     choice = models.ForeignKey(PollChoice)
     user = models.ForeignKey(User)
+    poll = models.ForeignKey(Poll, related_name="votes")
+    
+    class Meta:
+        unique_together = ('choice', 'user')
     
