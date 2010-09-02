5cab6461431a20e73bb32f8a75ee5ae4dd307bde
Corey Oordt <coordt@washingtontimes.com>
2010-09-02 06:09:19 -0400

    Refactored the views for the new urls

--- pollit/views.py
+++ pollit/views.py
@@ -1,15 +1,11 @@
+"""Request handling code for pollit"""
 import datetime
 from django.conf import settings
-from django.core.urlresolvers import reverse
-from django.contrib.admin.views.decorators import staff_member_required
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden, Http404
-from django.shortcuts import get_object_or_404, render_to_response
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.utils.safestring import mark_safe
-from django.views.decorators.cache import cache_page
-from django.utils.dates import MONTHS_3_REV
 
-from models import Poll, PollChoice, PollChoiceData, PollType
+from models import Poll, PollChoiceData, PollExpired
 
 def index(request, count=10, template_name="pollit/index.html"):
     """
@@ -20,61 +16,92 @@ def index(request, count=10, template_name="pollit/index.html"):
     return render_to_response(template_name,
                               {'poll_list': polls},
                               context_instance=RequestContext(request))
-                              
-def detail(request, year, month, slug, template_name="pollit/detail.html"):
-    try:
-        poll = Poll.objects.get(
-            pub_date__year=year, 
-            pub_date__month=MONTHS_3_REV[month],
-            slug=slug,
-            sites__pk__in=[settings.SITE_ID,])
-    except:
-        raise Http404
-        
-    if request.user.is_authenticated():
-        try:
-            PollChoiceData.objects.get(choice__poll__pk=poll.pk, user=request.user.pk)
-            has_voted = True
-        except:
-            has_voted = False
-    else:
-        has_voted = False
-        
-    if has_voted:
-        return HttpResponseRedirect("%sresults/" % poll.get_absolute_url())
-    # If user is logged in and has not voted
-    if request.method == "POST" and request.user.is_authenticated() and not has_voted:
-        try:
-            selected_choice = PollChoice.objects.get(pk=request.POST['choice'])
-        except PollChoice.DoesNotExist:
-            return HttpResponseRedirect(poll.get_absolute_url())
-            
-        selected_choice.votes += 1
-        selected_choice.save()
-        
-        PollChoiceData.objects.create(choice=selected_choice, user=request.user)
-        return HttpResponseRedirect("%sresults/" % poll.get_absolute_url()) 
+
+def detail_old(request, year, month, slug, template_name="pollit/detail.html"):
+    """A backwards-compatible way to handle the old urls"""
+    return detail(request, year, month, None, slug, template_name)
+
+def detail(request, year, month, day, slug, template_name="pollit/detail.html"):
+    """
+    Display the basic voting page. If the user has voted or is not authenticated
+    They cannot vote.
+    """
+    params = {
+        'pub_date__year': year,
+        'pub_date__month': datetime.datetime.strptime(month, '%b').month,
+        'slug': slug,
+        'sites__pk__in': [settings.SITE_ID,],
+    }
+    if day is not None:
+        params['pub_date__day'] = day
     
+    try:
+        poll = Poll.objects.get(**params)
+    except Poll.DoesNotExist, Poll.MultipleItemsReturned:
+        raise Http404
+    
+    errors = []
+    
+    # If user is logged in and has not voted
+    if request.method == "POST":
+        if request.user.is_authenticated():
+            try:
+                poll_choice = PollChoiceData.objects.get(
+                    poll__pk=poll.pk, 
+                    user__pk=request.user.pk)
+            except PollChoiceData.DoesNotExist:
+                poll_choice = None
+            
+            try:
+                poll.vote(request.POST['choice'], request.user)
+                return HttpResponseRedirect(poll.get_absolute_results_url())
+            except PollExpired:
+                errors.append('The poll has expired.')
+        else:
+            errors.append('You must be logged in to vote.')
+        
     return render_to_response(template_name,
                               {'poll': poll,
-                               'has_voted': has_voted},
+                               'has_voted': (poll_choice is not None),
+                               'user_choice': poll_choice,
+                               'errors': errors},
                               context_instance=RequestContext(request))
-                              
-def results(request, year, month, slug, template_name="pollit/results.html"):
+
+def results_old(request, year, month, slug, template_name="pollit/results.html"):
+    """A backwards-compatible way to handle the old urls"""
+    return results(request, year, month, None, slug, template_name)
+
+def results(request, year, month, day, slug, template_name="pollit/results.html"):
     """
     A simple view to show the results on a seperate url
     """
+    params = {
+        'pub_date__year': year,
+        'pub_date__month': datetime.datetime.strptime(month, '%b').month,
+        'slug': slug,
+        'sites__pk__in': [settings.SITE_ID,],
+    }
+    if day is not None:
+        params['pub_date__day'] = day
+    
     try:
-        poll = Poll.objects.get(
-            pub_date__year=year, 
-            pub_date__month=MONTHS_3_REV[month],
-            slug=slug,
-            sites__pk__in=[settings.SITE_ID,])
+        poll = Poll.objects.get(**params)
     except:
         raise Http404
-        
+    
+    poll_choice = None
+    if request.user.is_authenticated():
+        try:
+            poll_choice = PollChoiceData.objects.get(
+                choice__poll__pk=poll.pk, 
+                user=request.user.pk)
+        except PollChoiceData.DoesNotExist:
+            pass
+    
     return render_to_response(template_name,
-                              {'poll': poll},
+                              {'poll': poll,
+                              'has_voted': poll_choice is not None,
+                              'user_choice': poll_choice},
                               context_instance=RequestContext(request))
     
     
