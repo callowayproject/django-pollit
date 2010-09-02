e2f5c282be05b0d3d1c9a2f31ed858c3cb193c00
Corey Oordt <coordt@washingtontimes.com>
2010-09-02 14:37:52 -0400

    Improved the setup so it reflects the latest app skeleton version

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
+
+def read_file(filename):
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
 
 setup(name='pollit',
-      version='0.1.3',
-      description='',
-      long_description='',
-      author='Jose Soares',
-      author_email='jsoares@washingtontimes.com',
-      url='http://opensource.washingtontimes.com/projects/pollit/',
-      packages=['pollit'],
-      classifiers=['Framework :: Django',
-          'License :: OSI Approved :: Apache Software License',
-          'Development Status :: 4 - Beta',
-          'Environment :: Other Environment',
-          'Programming Language :: Python',
-          ],
-      )
+    version = __import__('pollit').get_version().replace(' ', '-'),
+    description='An application for creating polls and tabulating results',
+    long_description = read_file('README'),
+    author='Jose Soares',
+    author_email='jsoares@washingtontimes.com',
+    url='http://opensource.washingtontimes.com/projects/pollit/',
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires=read_file('requirements.txt'),
+    classifiers=['Framework :: Django',
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Programming Language :: Python',
+    ],
+)
