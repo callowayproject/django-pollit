eb85aad74695d96c4021adf27cfdb9f591519422
Jose Soares <josefsoares@gmail.com>
2011-08-30 10:33:49 -0400

    Updated the version to 0.3

--- pollit/__init__.py
+++ pollit/__init__.py
@@ -1,9 +1,9 @@
 __version_info__ = {
     'major': 0,
-    'minor': 2,
-    'micro': 3,
+    'minor': 3,
+    'micro': 0,
     'releaselevel': 'final',
-    'serial': 1
+    'serial': 0
 }
 
 def get_version():
