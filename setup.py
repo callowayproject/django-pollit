ea9f5733743bc8d1bd8c1acea9c98228ae7578ff
Jose Soares <josefsoares@gmail.com>
2011-08-30 10:32:27 -0400

    Updated the setup file

--- setup.py
+++ setup.py
@@ -1,28 +1,33 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
+import pollit
 
-def read_file(filename):
-    path = os.path.abspath(os.path.dirname(__file__))
-    filepath = os.path.join(path, filename)
-    try:
-        return open(filepath).read()
-    except IOError:
-        return ''
+version = pollit.__version__
 
-setup(name='pollit',
-    version = __import__('pollit').get_version().replace(' ', '-'),
+try:
+    f = open('README.rst')
+    long_desc = f.read()
+    f.close()
+except:
+    long_desc = ""
+
+try:
+    reqs = open('requirements.txt').read()
+except:
+    reqs = ''
+
+setup(name='django-pollit',
+    version = version,
     description='An application for creating polls and tabulating results',
-    long_description = read_file('README'),
+    long_description = long_desc,
     author='Jose Soares',
-    author_email='jsoares@washingtontimes.com',
-    url='http://opensource.washingtontimes.com/projects/pollit/',
+    author_email='jose@linux.com',
+    url='https://github.com/callowayproject/django-pollit',
     packages = find_packages(),
     include_package_data = True,
-    install_requires=read_file('requirements.txt'),
+    install_requires = reqs,
     classifiers=['Framework :: Django',
         'License :: OSI Approved :: Apache Software License',
-        'Development Status :: 4 - Beta',
-        'Environment :: Other Environment',
         'Programming Language :: Python',
     ],
 )
